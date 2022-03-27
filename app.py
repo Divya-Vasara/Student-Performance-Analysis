b21f9e495652d3a005f180c543f9b3b93550307e
Divya Vasara <81897724+Divya-Vasara@users.noreply.github.com>
2022-03-27 10:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,56 @@
-from flask import Flask, render_template, request
-
-import requests
-import pickle
+from flask import Flask, render_template, url_for, request
+from sklearn.externals import joblib
+import os
 import numpy as np
-import sklearn
+import pickle
+import time
+import graphviz
+import pandas as pd
+import os
+from sklearn.ensemble import BaggingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import sklearn.metrics
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from model import * 
+from sklearn import linear_model
+app = Flask(__name__, static_folder='static')
 
-app = Flask(__name__)
-model = pickle.load(open("df.pkl", 'rb'))
-
-@app.route('/',methods=['GET'])
-def Home():
-    return render_template('index.html')
-
-
-@app.route("/predict", methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        Gender = request.form['Gender']
-        if Gender == "Male":
-            Gender = 1
-        else:
-            Gender = 0
-
-        Race=request.form['Race/ethnicity']
-        if(Race=='Group A'):
-            Race = 0
-        elif(Race=="Group B"):
-            Race = 1 
-        elif(Race=="Group C"):
-            Race = 2
-        elif(Race=="Group D"):
-            Race= 3
-        else:
-            Race = 4
-        
-        Parental_Level_of_Education = request.form['Parental_Level_of_Education']
-        if Parental_Level_of_Education == "some high school":
-            Parental_Level_of_Education = 5
-        elif Parental_Level_of_Education =="high school":
-            Parental_Level_of_Education =4
-        elif Parental_Level_of_Education =="associate's degree":
-            Parental_Level_of_Education = 3
-        elif Parental_Level_of_Education == "master's degree":
-            Parental_Level_of_Education = 2
-        elif Parental_Level_of_Education == "some college":
-            Parental_Level_of_Education = 1
-        else :
-            Parental_Level_of_Education = 0 
+@app.route("/")
+def index():
+    return render_template('home.html')
 
 
-        Math_score = int(request.form['Math_Score'])
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    dbms = int(request.form['dbms'])
+    osa = int(request.form['osa'])
+    lac = int(request.form['lac'])
+    se = int(request.form['se'])
+    mea = int(request.form['mea'])
+    x = np.array([dbms]).reshape(1, -1)
+    final = []
+    first=first_subject(dbms)
+    second=second_subject(osa)
+    third=third_subject(lac)
+    fourth=fourth_subject(se)
+    fifth=fifth_subject(mea)
+    print("dddddddddddddddddd")
+    print(final) 
+    
+    
+    return render_template('Result.htm', first=int(first),second=int(second),third=int(third),fourth=int(fourth),fifth=int(fifth))
 
-        Reading_score = int(request.form['Reading_score'])
 
-        Writing_score = int(request.form['Writing_score'])
-
-
-        Lunch = request.form['Lunch']
-        if(Lunch=="standard"):
-            Lunch = 1
-        else:
-            Lunch = 0
-        
-        test_preparation_course = request.form['test_preparation_course']
-        if test_preparation_course == "None":
-            test_preparation_course = 1
-        else:
-            test_preparation_course = 0
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
-      
-        prediction=model.predict(([[Gender,Race,Parental_Level_of_Education,Math_score,Reading_score,Writing_score,Lunch,test_preparation_course]]))
-        print(prediction)
-        prediction=np.round(prediction[0],2)
-        return render_template('index.html',Average=prediction)
-    else:
-        return render_template('index.html')
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
